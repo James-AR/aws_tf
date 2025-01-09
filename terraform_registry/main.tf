6e626ed6072d80c5572c0c8322d2af2de4b9927b
James-AR <ruiz.james12@yahoo.com>
2025-01-08 18:09:49 -0800

    Adding security group module

--- terraform_registry/main.tf
+++ terraform_registry/main.tf
@@ -25,3 +25,13 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "ssh_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/ssh"
+
+  name        = "ssh"
+  description = "Security group for SSH ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
